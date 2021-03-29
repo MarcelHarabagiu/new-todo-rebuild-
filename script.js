8185ecbb85e6812da03c0ee82443f3fdff9ff805
Marcel Ionut Harabagiu <ionutharabagiu78@gmail.com>
2021-03-29 16:56:05 +0200

    small correction

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ function addTodo(event) {
   event.preventDefault();
   const todoDiv = document.createElement('div');
   todoDiv.classList.add('todo');
-  const newTodo = document.createElement("li");
+  const newTodo = document.createElement('li');
   newTodo.innerText = todoInput.value;
-  newTodo.classList.add("todo-item");
+  newTodo.classList.add('todo-item');
   todoDiv.appendChild(newTodo);
   todoList.appendChild(todoDiv);
   todoInput.value = ''
