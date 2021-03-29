8757672b9aa1c4cf3804954b32e8da2fa3061f4c
Marcel Ionut Harabagiu <ionutharabagiu78@gmail.com>
2021-03-29 16:13:30 +0200

    starting a new todo app

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let todoInput;
+let todoButton;
+let todoList;
+
+todoInput = document.querySelector('.todo-input');
+todoButton = document.querySelector('.todo-button');
+todoList = document.querySelector('.todo-list');
+todoButton.addEventListener('click', addTodo);
+
+function addTodo(event) {
+  event.preventDefault();
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  todoList.appendChild(todoDiv);
+  todoInput.value = ''
+}
