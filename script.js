9d390e101967be7dde905cc0dce439805492413f
Marcel Ionut Harabagiu <ionutharabagiu78@gmail.com>
2021-04-07 21:42:12 +0200

    indent

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 
 class Main {
 
-const todoItem = new Main(todoText)
+    let dom = () => {
+      let todoButton = document.querySelector('.todo-button');
+      todoButton.addEventListener('click', addTodo);
+    }
 
-let dom = () => {
-  let todoButton = document.querySelector('.todo-button');
-  todoButton.addEventListener('click', addTodo);
-}
+    let addTodo = () => {
+      const todoInput = getTodoInput();
+      const todoText = todoInput.value;
+      const todoItem = new TodoItem(todoText);
+      modelTodos.push(todoItem);
+      todoInput.value = '';
+      createElement(todoItem);
+    }
+    let getTodoInput = () => document.querySelector('.todo-input');
+    let getTodoList = () => document.querySelector('.todo-list');
 
-let addTodo = () => {
-  const todoInput = getTodoInput();
-  const todoText = todoInput.value;
-  const todoItem = new TodoItem(todoText);
-  modelTodos.push(todoItem);
-  todoInput.value = '';
-  createElement(todoItem);
-}
-let getTodoInput = () => document.querySelector('.todo-input');
-let getTodoList = () => document.querySelector('.todo-list');
-
-let createElement = (newTodoObj) => {
-  const todoDiv = document.createElement('div');
-  todoDiv.classList.add('todo');
-  const todoLi = document.createElement('li');
-  todoLi.innerText = newTodoObj.text;
-  todoLi.classList.add('todo.item');
-  todoDiv.appendChild(todoLi);
-  getTodoList().appendChild(todoDiv);
-}
-const modelTodos = [];
+    let createElement = (newTodoObj) => {
+      const todoDiv = document.createElement('div');
+      todoDiv.classList.add('todo');
+      const todoLi = document.createElement('li');
+      todoLi.innerText = newTodoObj.text;
+      todoLi.classList.add('todo.item');
+      todoDiv.appendChild(todoLi);
+      getTodoList().appendChild(todoDiv);
+    }
+    const modelTodos = [];
 
 }
 
