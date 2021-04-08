f0e111e2e00343eafb2d3f6ad244749eacea7659
Marcel Ionut Harabagiu <ionutharabagiu78@gmail.com>
2021-04-08 23:50:00 +0200

    changes in class

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
 
 class Main {
 
-    let dom = () => {
-      let todoButton = document.querySelector('.todo-button');
-      todoButton.addEventListener('click', addTodo);
-    }
+  modelTodos;
+  todoButton;
+  todoInput;
 
-    let addTodo = () => {
-      const todoInput = getTodoInput();
-      const todoText = todoInput.value;
-      const todoItem = new TodoItem(todoText);
-      modelTodos.push(todoItem);
-      todoInput.value = '';
-      createElement(todoItem);
-    }
-    let getTodoInput = () => document.querySelector('.todo-input');
-    let getTodoList = () => document.querySelector('.todo-list');
 
-    let createElement = (newTodoObj) => {
-      const todoDiv = document.createElement('div');
-      todoDiv.classList.add('todo');
-      const todoLi = document.createElement('li');
-      todoLi.innerText = newTodoObj.text;
-      todoLi.classList.add('todo.item');
-      todoDiv.appendChild(todoLi);
-      getTodoList().appendChild(todoDiv);
-    }
-    const modelTodos = [];
+  dom = () => {
+  this.todoButton = document.querySelector('.todo-button');
+  this.todoButton.addEventListener('click', this.addTodo);
+}
+
+   addTodo = () => {
+    const todoInput = getTodoInput();
+    const todoText = this.todoInput.value;
+    const todoItem = new TodoItem(todoText);
+    this.modelTodos.push(todoItem);
+    this.todoInput.value = '';
+    this.createElement(todoItem);
+  }
+  let getTodoInput = () => document.querySelector('.todo-input');
+  let getTodoList = () => document.querySelector('.todo-list');
+
+  let createElement = (newTodoObj) => {
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    const todoLi = document.createElement('li');
+    todoLi.innerText = newTodoObj.text;
+    todoLi.classList.add('todo.item');
+    todoDiv.appendChild(todoLi);
+    this.getTodoList().appendChild(todoDiv);
+  }
+  const modelTodos = [];
 
 }
 
 const main = new Main()
 window.onload = main.dom;
-
 
 
 class TodoItem {
